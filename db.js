8ad1b3e7d8855943842ad8af23ed01c4c95cb095
bys42 <pumapupa@gmail.com>
2019-11-22 21:02:49 +0800

    1. add db init (firebase) 2. db trial, show title string in db

--- db.js
+++ db.js
@@ -1,1 +1,14 @@
-// TODO: 初始化db
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://helloexpressjs.firebaseio.com"
+});
+
+// init db
+const db = admin.firestore();
+
+//export db
+module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const db = require('../db');
 const router = express.Router();
 
 // 首頁路由
-router.get('/', function (req, res, next) {
-  // TODO: 取得產品列表
-  res.render('index');
+router.get('/', async function (req, res, next) {
+  // get doc:home from set:content (in firebase)
+  const doc = await db.doc('content/home').get();
+  
+  res.locals.title = doc.data().title;
+  res.locals.subtitle = doc.data().subtitle;
+  
+  res.render('index'); // use template index.ejs
 });
 
 module.exports = router;
