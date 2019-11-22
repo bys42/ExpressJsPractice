4cd81722856d77c40dfe3887500c165f0a5fe14f
bys42 <pumapupa@gmail.com>
2019-11-22 21:12:07 +0800

    1. mode create product api to add db item

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db');
 
 // 登入
 router.post('/login', function (req, res, next) {
@@ -13,9 +14,22 @@ router.post('/logout', function (req, res, next) {
 
 // 新增商品
 router.post('/product/create', function (req, res, next) {
-    res.status(200).json ({
-        msg: '[CreateProduct] Operation Complete'
-    });
+    const product = req.body;
+    db
+        .collection('products')
+        .add(product)
+        .then(responce => { // after operation complete properly
+            res.status(200).json({
+                msg: 'Create Product Success',
+                responce: responce
+            });
+        })
+        .catch(err => {  // if operation failed
+            res.status(500).json({
+                msg: 'Create Product Failed, please try again',
+                responce: err
+            });
+        })
 });
 
 // 更新商品
