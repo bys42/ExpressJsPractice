77fe22d36a24a29431839601781a76df182ec854
BYS <pumapupa@gmail.com>
2019-12-03 14:56:00 +0800

    1. add signup function 2. upgrade firebase script 7.5.0

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -22,20 +22,45 @@ $('#loginForm').submit(function (event) {
                 })
         })
         .catch(function (err) {
-            alert(err);
+            alert('Login Failed', err);
             console.log('Login Failed', err);
         });
 });
 
+function checkPassword(event) {
+    const password = $('#signUpPassword').val();
+    const confirmPw = $('#comfirmPassword').val();
+    if (password === confirmPw) {
+        document.getElementById("comfirmPassword").setCustomValidity("");;
+    } else {
+        document.getElementById("comfirmPassword").setCustomValidity("Passwords Don't Match");;
+    }
+}
+
+$('#comfirmPassword').on('keyup', checkPassword);
+$('#signUpPassword').on('keyup', checkPassword);
+
 $('#signUpForm').submit(function (event) {
     event.preventDefault();
+    const email = $('#signUpEmail').val();
+    const password = $('#signUpPassword').val();
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(function (res) {
+            alert('Signin Success');
+            window.location = '/';
+        })
+        .catch(function (err) {
+            alert('Signin Failed');
+            console.log(err);
+        });
 });
 
 // 登出按鈕點擊時
 $('#logoutBtn').click(function () {
     axios.post('/api/logout', {})
         .then(function (res) {
-            alert('Logout')
             window.location = '/';
         })
         .catch(function (err) {
